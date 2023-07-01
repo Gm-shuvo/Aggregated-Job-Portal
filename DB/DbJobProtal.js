9ad73dfab50d5ac68a4712aa2bbc093cb783c6cb
Gm-shuvo <gm.shuvo.ru.cse@gmail.com>
2023-07-02 00:45:59 +0600

    changes

--- DB/DbJobProtal.js
+++ DB/DbJobProtal.js
@@ -2,7 +2,7 @@ import {connectDB} from './connectDB.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log("process.env.DBSCRAPY_URL", process.env.DBSCRAPY_URL);
+// console.log("process.env.DBSCRAPY_URL", process.env.DBSCRAPY_URL);
 
 const connectDBJobPortal = async () => {
    await connectDB(process.env.DBSCRAPY_URL);
